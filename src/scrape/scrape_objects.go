a3b378cfacc4dcb920bacc7cf57f97b9799d446e
adriankr38 <adrian58ago@gmail.com>
2023-04-22 17:56:55 +0200

    fix(go): Columns to csv

--- src/scrape/scrape_objects.go
+++ src/scrape/scrape_objects.go
@@ -2,6 +2,8 @@ package scrape;
 
 import(
   "fmt"
+  "encoding/csv"
+  "os"
 )
 
 type Column struct{
@@ -30,4 +32,59 @@ func (c *Column) AddValue(v interface{}){
 
 func (c *Column) GetValues()[]interface{}{
   return c.Values
+}
+
+func (c *Column) ExportColumnsToCsv(columns []Column, filename string) error{
+  if len(columns) == 0 {
+    return fmt.Errorf("Empty column slice")
+  }
+
+  file,err := os.Create(filename)
+  if err != nil{
+      fmt.Printf("Error creating csv -> %v\n",err)
+      return err
+  }
+
+  defer file.Close()
+
+  writer := csv.NewWriter(file)
+
+  header := make([]string, len(columns))
+  for i, col:= range columns{
+    header[i] = col.ColName
+  }
+  fmt.Println(header)
+  err = writer.Write(header)
+  if err != nil{
+      fmt.Printf("Error writing header -> %v\n",err)
+      return err 
+  }
+
+  var maxRows int = 0;
+  for _,col := range columns{
+    if len(col.Values) > maxRows{
+      maxRows = len(col.Values)
+    }
+
+  }
+  rows := make([][]string,maxRows)
+  for i := 0; i< maxRows; i++{
+    row := make([]string, len(columns))
+    for j,col := range columns{
+      if i >= len(col.Values){
+         row[j] = "NA"
+      }else{
+        row[j] = col.Values[i].(string)
+    }
+
+    }
+
+    rows = append(rows, row)
+    err = writer.Write(row)
+
+  }
+
+  writer.Flush()
+  return nil
+
 }
